836a933d753e0f6576de0def27b51b4065beb040
Gazel <gazel@sau.ru>
2022-06-29 14:31:30 +0300

    Updated OpenCV and clearml service

--- scripts/init_env.sh
+++ scripts/init_env.sh
@@ -11,7 +11,7 @@ export LD_LIBRARY_PATH=$INSTALL_DIR/lib:$LD_LIBRARY_PATH
 # source $PWD/dist/bin/setup_vars_opencv4.sh
 
 export OPEN3D_VERSION=v0.13.0
-export OPENCV_VERSION=4.5.3
+export OPENCV_VERSION=4.6.0
 export CMAKE_VERSION=v3.21.3
 # Set this from get_compute_arch.sh script
 export CUDA_ARCH=6.1
