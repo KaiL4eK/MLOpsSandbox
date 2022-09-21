b29a9830a1ff78dce424222cdb1686e0e8226e56
Aleksey Devyatkin <aleksey_devyatkin@Aleksey.Devyatkin-pc>
2022-09-21 13:50:31 +0300

    Added JL image start

---
+++ jupyterlab/scripts/start.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -o allexport; source .env; set +o allexport
+
+DOCKER_OPTS=
+
+while getopts d flag
+do
+    case "${flag}" in
+        # Daemonize start
+        d) DOCKER_OPTS=-d;;
+    esac
+done
+
+echo "Docker options: $DOCKER_OPTS"
+echo "Compose project name: $COMPOSE_PROJECT_NAME"
+echo "Compose root dir: $COMPOSE_ROOT_DIR"
+
+mkdir -p $COMPOSE_ROOT_DIR/data
+if [ ! $? = 0 ]; then
+    echo "Failed to create directories"
+    exit 1
+fi
+
+docker-compose stop && \
+docker-compose up --build $DOCKER_OPTS
