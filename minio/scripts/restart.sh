3b36627121c5f11c912dd5aa483a311986e62872
Aleksey Devyatkin <aleksey_devyatkin@Aleksey.Devyatkin-pc>
2022-12-28 12:50:06 +0300

    Added minio storage compose

---
+++ minio/scripts/restart.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -o allexport; source .env; set +o allexport
+
+DOCKER_OPTS=
+
+while getopts d flag
+do
+    case "${flag}" in
+        # Daemonize start
+        d) DOCKER_OPTS=-d;;
+    esac
+done
+
+echo "Docker options: $DOCKER_OPTS"
+echo "Compose project name: $COMPOSE_PROJECT_NAME"
+
+mkdir -p data
+
+docker-compose stop && \
+docker-compose up $DOCKER_OPTS
